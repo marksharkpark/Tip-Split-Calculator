5d251458168d9fdc5b7088c6a07ed2e73fdfe61b
marksharkpark <39388430+marksharkpark@users.noreply.github.com>
2021-07-13 10:48:12 -0700

    Final Commit
    Outputting results onto ResultViewController.
    
    More detailed comments on functions and portions of code.
    
    Undesired Behavior Fixes:
    - Output consists with two decimal points and dollar sign.
    - Outputting requested calculation along with number of people and tip percentage.

--- Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -17,11 +17,12 @@ class CalculatorViewController: UIViewController {
     
     var calcBrain = calculatorBrain()
     
-    
+    // Evaluating which tip was chosen. 
     @IBAction func tipChanged(_ sender: UIButton) {
       //  billTextField.endEditing(true)
         if sender.currentTitle == "20%"{
             percentCheck(check: "2")
+            // button animation
             twentyPctButton.isSelected = true
             tenPctButton.isSelected = false
             zeroPctButton.isSelected = false
@@ -29,12 +30,14 @@ class CalculatorViewController: UIViewController {
         }
         else if sender.currentTitle == "10%"{
             percentCheck(check: "1")
+            // button animation
             tenPctButton.isSelected = true
             twentyPctButton.isSelected = false
             zeroPctButton.isSelected = false
         }
         else{
             percentCheck(check: "0")
+            // button animation
             zeroPctButton.isSelected = true
             twentyPctButton.isSelected = false
             tenPctButton.isSelected = false
@@ -63,34 +66,48 @@ class CalculatorViewController: UIViewController {
         }
     }
     
+    // IBAction func:
+    //      Incrementing stepper value and displaying. Also saving the value to include
+    //      within resultviewcontroller
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
         calcBrain.numPeople = sender.value
         splitNumberLabel.text = String(format: "%.0f", sender.value)
     }
     
+    // IBAction func:
+    //      Calculation of split cost among x people, then performing transition to
+    //      resultviewcontroller
     @IBAction func calculatePressed(_ sender: UIButton) {
         let cost: Double = Double(billTextField.text!) ?? 0.0
         calcBrain.price = cost
-        calcBrain.output = (calcBrain.price / calcBrain.numPeople) * calcBrain.percent
-        self.performSegue(withIdentifier: "goToResult", sender: self)
-            
-    
- /*       if calcBrain.zero == true{
-            let tip = calcBrain.price * calcBrain.percent
-            print(tip)
-        }
-        else if calcBrain.ten == true{
-            print(calcBrain.percent)
+        print(calcBrain.zero)
+        if calcBrain.zero == false{
+            calcBrain.output = (calcBrain.price / calcBrain.numPeople) * calcBrain.percent
         }
         else{
-            print(calcBrain.percent)
-        } */
-        
+            calcBrain.output = (calcBrain.price / calcBrain.numPeople)
+        }
+        self.performSegue(withIdentifier: "goToResult", sender: self)
+                    
     }
     
-    
-
-
-
+    // Helper function to prepare for the next segueway with appropriate data
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToResult"{
+            let destinationVC = segue.destination as! ResultsViewController
+            destinationVC.splitCost = String(format: "%0.02f", calcBrain.output)
+            
+            
+            if calcBrain.zero == true{
+                destinationVC.advice = "Splitting bill amongst " + String(format: "%.0f", calcBrain.numPeople) + " people, with " + "0" + "% tip."
+            }
+            else if calcBrain.ten == true{
+                destinationVC.advice = "Splitting bill amongst " + String(calcBrain.numPeople) + " people, with " + "10" + "% tip."
+            }
+            else{
+                destinationVC.advice = "Splitting bill amongst " + String(calcBrain.numPeople) + " people, with " + "20" + "% tip."
+            }
+        }
+    }
 }
 
--- Tipsy/Controller/ResultsViewController.swift
+++ Tipsy/Controller/ResultsViewController.swift
@@ -13,24 +13,22 @@ class ResultsViewController: UIViewController {
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var settingsLabel: UILabel!
     
+    var splitCost : String = "Error"
+    var advice : String = "Splitting bill amongst x people, with x% tip."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        totalLabel.text = "$" + splitCost
+        settingsLabel.text = advice
 
         // Do any additional setup after loading the view.
     }
     
 
     @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
         
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
